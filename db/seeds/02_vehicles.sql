eb30934640e923eb88f6df5759e76c32e4b08231
toddteigland <toddteigland@gmail.com>
2022-12-18 22:35:53 -0800

    Created vehicle schema, and add user and vehicle data to tables

---
+++ db/seeds/02_vehicles.sql
@@ -0,0 +1,20 @@
+INSTERT INTO vehicles (owner_id, make, model, yr, color, price, likes, thumbnail_img, fullsize_img)
+  VALUES (4, 'Honda', 'Prelude', 1989, 'white', 1800, 4, 'https://live.staticflickr.com/65535/49933628983_59bb74f402_b.jpg','https://bringatrailer.com/wp-content/uploads/2020/02/1989_honda_prelude_15844948327401f0611b9HondaPrelude_4WS-2.jpg?fit=940%2C626',  );
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (2, 'Chevrolet', 'S10', 1989, 'white', 2000, 7, 'http://scorpionagency.com/blog/wp-content/uploads/2016/09/1989-checy-s10-3-300x169.jpg', 'https://cs.copart.com/v1/AUTH_svc.pdoc00001/LPP366/f3fed24cd68e454bab8b1331582e1d6c_ful.jpg'  );
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (3, 'Mazda', 'Protoge', 1992, 'red', 2400, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmVFpyoX5fvW5ISmREv0tHqmU8NlhxOF8Z-A&usqp=CAU', 'https://upload.wikimedia.org/wikipedia/commons/8/83/1990-1992_Mazda_Protege_--_06-03-2011.jpg' );
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (5 'Volkswagen', 'Golf', 2004, 'blue', 3200, 7, 'https://imganuncios.mitula.net/medium/volkswagen_golf_2008_12_788_2008_shadow_blue_metallic_volkswagen_golf_9070096668347130150.jpg', 'https://media.ed.edmunds-media.com/volkswagen/golf/2000/oem/2000_volkswagen_golf_4dr-hatchback_gls-18t_fq_oem_2_500.jpg'  );
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (6, 'Jeep', 'Cherokee', 2016, 'grey', 27500, 6, 'https://img2.autoaubaine.com/cb1668790302/cars/1924/8658544/1/Jeep-Cherokee-2016.jpg', 'https://www.platinumautohaus.com/imagetag/13675/7/l/Used-2016-Jeep-Cherokee-Trailhawk.jpg');
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (7, 'Tesla', 'Model 3', 2019, 'white', 62799, 9, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0jerT9lvo0qQJMKpsE6S8p08G54qUeHJWZg&usqp=CAU', 'https://cdn.motor1.com/images/mgl/OW9Ge/s1/white-model-3.jpg');
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (8, 'Rivian', 'R1S', 2022, 'green', 126000, 5, 'https://d2q97jj8nilsnk.cloudfront.net/images/r1t-1.jpeg', 'https://cdn.motor1.com/images/mgl/pLK4o/s1/rivian-r1t-forest-green.jpg');
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (9, 'Aston Martin', 'DB11', 2018, 'black', 287999, 4,'https://www.autoguide.com/blog/wp-content/gallery/2018-aston-martin-db11-v8-review-9-26-2017/2018-Aston-Martin-DB11-V8-Front-07.jpg', 'https://www.autoguide.com/blog/wp-content/gallery/2018-aston-martin-db11-v8-review-9-26-2017/2018-Aston-Martin-DB11-V8-Rear-01.jpg');
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (5, 'Toyota', 'Corolla', '2004', 'beige', 1300, 'https://carfax-img.vast.com/carfax/v2/-4064455723145242707/1/344x258','https://www.edmunds.com/assets/m/for-sale/97-2t1br32ex7c841218/img-1-600x400.jpg');
+  INSTERT INTO vehicles (owner_id, make, model, yr, color, likes, thumbnail_img, fullsize_img)
+  VALUES (7, 'Volskwagen', 'Westfalia', 1970, 'green', 7900, 6, 'https://c8.alamy.com/comp/2F2BYA4/classic-green-and-white-vw-camper-van-parked-on-seafront-promenade-2F2BYA4.jpg', 'https://assets.newatlas.com/dims4/default/081d3d1/2147483647/strip/true/crop/1620x1080+150+0/resize/1200x800!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2Fvolkswagen-samba-microbus-record-price-3.jpg');
