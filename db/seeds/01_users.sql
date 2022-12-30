beab8e715997bcc83bb8f4391f1c355ba93c4d66
toddteigland <toddteigland@gmail.com>
2022-12-30 12:41:58 -0800

    Forgot quotes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,12 +1,12 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email) VALUES ('Kira', Kira@email.com);
-INSERT INTO users (name, email) VALUES ('Jeff', Jeff@email.com);
-INSERT INTO users (name, email) VALUES ('Alice', Alice@email.com);
-INSERT INTO users (name, email) VALUES ('Tara', Tara@email.com);
-INSERT INTO users (name, email) VALUES ('Dan', Dan@email.com);
-INSERT INTO users (name, email) VALUES ('Billy', Billy@email.com);
-INSERT INTO users (name, email) VALUES ('Sarah', Sarah@email.com);
-INSERT INTO users (name, email) VALUES ('Maureen', Maureen@email.com);
-INSERT INTO users (name, email) VALUES ('Claire', Claire@email.com);
-INSERT INTO users (name, email) VALUES ('Hugh', Hugh@email.com);
+INSERT INTO users (name, email) VALUES ('Kira', 'Kira@email.com');
+INSERT INTO users (name, email) VALUES ('Jeff', 'Jeff@email.com');
+INSERT INTO users (name, email) VALUES ('Alice', 'Alice@email.com');
+INSERT INTO users (name, email) VALUES ('Tara', 'Tara@email.com');
+INSERT INTO users (name, email) VALUES ('Dan', 'Dan@email.com');
+INSERT INTO users (name, email) VALUES ('Billy', 'Billy@email.com');
+INSERT INTO users (name, email) VALUES ('Sarah', 'Sarah@email.com');
+INSERT INTO users (name, email) VALUES ('Maureen', 'Maureen@email.com');
+INSERT INTO users (name, email) VALUES ('Claire', 'Claire@email.com');
+INSERT INTO users (name, email) VALUES ('Hugh', 'Hugh@email.com');
 
