f30f975ca3eae266cde9938443fef8093a5832d8
toddteigland <toddteigland@gmail.com>
2023-01-01 17:56:38 -0800

    added a sort drop-down, still need to config it

--- db/queries/index.js
+++ db/queries/index.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 const getMostLiked = () => {
   return db.query(
-    'SELECT * FROM vehicles JOIN users ON users.id = owner_id ORDER BY likes DESC LIMIT 5;')
+    'SELECT * FROM vehicles JOIN users ON users.id = owner_id;')
     .then(response => {
       return response.rows;
     });
