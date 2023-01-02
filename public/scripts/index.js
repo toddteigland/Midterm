1feee9b96046795a62ec181ddc76a241c4985779
toddteigland <toddteigland@gmail.com>
2023-01-01 20:32:53 -0800

    Finished sort drop-down function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,29 +9,44 @@ $(() => {
         const $vehicleList = $('#most-liked');
         console.log('RESPONSE:', response);
 
-        // const likedFilter = response.filter((vehicle) => vehicle.make === "Honda");
-        // console.log('honda', likedFilter)
-
         //On page load vehicles are sorted by likes
         response.vehicles.sort((a, b) => b.likes - a.likes);
 
         //Using drop down, sort by the selected option
         const sort = document.querySelector('#sort');
         sort.addEventListener('change', (event) => {
-          const result = document.querySelector('#sort');
-          response.vehicles.sort((a, b) => b.result - a.result);
+          const click = event.target.value;
+          if (click === 'price-asc') {
+            response.vehicles.sort((a, b) => a.price - b.price);
+          }
+          else if (click === 'price-desc') {
+            response.vehicles.sort((a,b) => b.price - a.price);
+          }
+          else if (click === 'year-asc') {
+            response.vehicles.sort((a,b) => a.yr - b.yr);
+          }
+          else if (click === 'year-desc') {
+            response.vehicles.sort((a,b) => b.yr - a.yr);
+          }
+          else if (click === 'likes') {
+            response.vehicles.sort((a,b) => b.likes - a.likes);
+          }
 
+          $vehicleList.html("")
+
+          for (const vehicle of response.vehicles) {
+            $(`<li class="most-liked">`).html(
+              // Width + Height are just here temporarily until css is done!!!!!!!!!
+              `<img src="${vehicle.thumbnail_img}" width="100px" height="100px"/>
+            <p> ${vehicle.yr} </p>
+            <p> ${vehicle.make} <b>${vehicle.model}</b></p>
+            <p> $${vehicle.price} </p>
+            <p> # of LIKES = ${vehicle.likes}</p>
+            <p>Contact Owner: <a href="mailto: ${vehicle.email}">${vehicle.email}</a></p>`
+            ).appendTo($vehicleList);
+          }
 
         });
-
-        switch (sort) {
-          case '':
-
-            break;
-          case '':
-
-            break;
-        }
 
         // Gives each item a class for styling and adds to vehicle list
         for (const vehicle of response.vehicles) {
