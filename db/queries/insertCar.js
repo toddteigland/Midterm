63c6c4267a2093b42eadcc75952e32d321252462
toddteigland <toddteigland@gmail.com>
2023-01-06 00:42:16 -0800

    added like function, delete listings function

--- db/queries/insertCar.js
+++ db/queries/insertCar.js
@@ -2,8 +2,8 @@ const db = require('../connection');
 
 const saveToDatabase = (make, model, yr, color, price, thumbnail_img, fullsize_img) => {
   // inserted into the vehicle table
-  const query = `INSERT INTO vehicles (make, model, yr, color, price, thumbnail_img, fullsize_img) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-  return db.query(query, [make, model, yr, color, price, thumbnail_img, fullsize_img])
+  const query = `INSERT INTO vehicles (owner_id, make, model, yr, color, price, thumbnail_img, fullsize_img) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+  return db.query(query, [1, make, model, yr, color, price, thumbnail_img, fullsize_img])
     .then(response => {
       console.log("Vehicle saved to database");
     })
@@ -12,4 +12,16 @@ const saveToDatabase = (make, model, yr, color, price, thumbnail_img, fullsize_i
     })
 };
 
-module.exports = { saveToDatabase }
+const addFirstLike = (user_id, vehicle_id) => {
+  const query = `INSERT INTO likes (user_id, vehicle_id)
+  VALUES($1,$2)`
+  return db.query(query, [1,11])
+    .then(response => {
+      console.log('FIRST LIKE ADDED!');
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+};
+
+module.exports = { saveToDatabase, addFirstLike }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router  = express.Router();
 const { userLikes } = require('../db/queries/likedCars');
 const { userCars } = require('../db/queries/userCars');
-const { saveToDatabase } = require('../db/queries/insertCar')
+const { saveToDatabase, addFirstLike } = require('../db/queries/insertCar')
 const app = express()
 const bodyParser = require('body-parser');
 const { response } = require('express');
-const { markedSold } = require('../db/queries/markAsSold');
+const { markedSold, deleteVehicle } = require('../db/queries/markAsSold');
 
 app.use('/api/userLikes', userLikes)
 app.use('/api/userCars', userCars)
@@ -19,16 +19,22 @@ router.get('/', async(req, res) => {
   res.render('dashboard', { likedCars, ownerCars, userEmail: req.cookies.userEmail});
 }); 
 
-router.post('/api/cars/:id', async(req, res) => {
-  console.log("MarkSold", req.params.id)
-  markedSold(req.params.id)
-})
+// router.post('/api/cars/:id', async(req, res) => {
+//   console.log("MarkSold", req.params.id)
+//   markedSold(req.params.id)
+// })
+
+
 
 router.post('/api/cars', async(req, res) => {
-  console.log(req.body)
-  saveToDatabase(req.body.make, req.body.model, req.body.yr, req.body.color, req.body.price, 1, req.body.thumbnail_img, req.body.fullsize_img).then(() => {
+  saveToDatabase(req.body.make, req.body.model, req.body.yr, req.body.color, req.body.price, req.body.thumbnail_img, req.body.fullsize_img)
+  .then(() => {
+    addFirstLike(1, 11);
     res.json({ status: "success" })
   });
+    // .then(() => {
+    //   res.json({ status: 'Success'})
+    // });
 })
 
 
